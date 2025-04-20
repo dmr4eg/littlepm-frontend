0b12775fd361972923fab771364d31dfe0bfd8f4
Damir <fifth.dmr@gmail.com>
2025-04-20 22:13:34 +0000

    the day progress bar is not shown on the day 6 page

--- src/app/day-6/page.tsx
+++ src/app/day-6/page.tsx
@@ -33,7 +33,58 @@ const DaySixPage = () => {
       <Header/>
 
       <main className="flex-grow p-8">
-        <div className="bg-[#FAF0E6] rounded-3xl p-8 flex flex-col gap-6">
+        <div className="bg-[hsl(var(--secondary))] rounded-3xl p-8 flex flex-col gap-6">
+          <div className="flex items-center gap-2">
+            <Image
+              src="https://icons.veryicon.com/png/o/miscellaneous/3d-element-icon/cube-48.png"
+              alt="Cat Toy"
+              width={24}
+              height={24}
+            />
+            <div className="text-xl">You are doing a &quot;Cat Toy&quot; project with the DIY</div>
+          </div>
+
+          <div className="flex justify-center relative">
+            {/* Timeline */}
+            <div className="absolute top-1/2 left-0 right-0 h-[2px] bg-border z-0"/>
+            {/* Stars */}
+            {Array.from({length: 7}).map((_, index) => {
+              const day = index + 1;
+              const isActive = day === 6;
+              const isCompleted = day < 6;
+              return (
+                <div key={index} className="relative z-10">
+                  {isActive ? (
+                    <Image
+                      src="https://clipart-library.com/images_k/star-clipart-transparent/star-clipart-transparent-12.png"
+                      alt={`Day ${day}`}
+                      width={48}
+                      height={48}
+                      className="w-12 h-12"
+                    />
+                  ) : isCompleted ? (
+                    <Image
+                      src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Check_green_icon.svg/1200px-Check_green_icon.svg.png"
+                      alt={`Day ${day}`}
+                      width={32}
+                      height={32}
+                      className="opacity-100"
+                    />
+                  ) : (
+                    <Image
+                      src="https://static.vecteezy.com/system/resources/previews/009/734/993/original/cute-star-clipart-design-free-png.png"
+                      alt={`Day ${day}`}
+                      width={48}
+                      height={48}
+                      className="w-12 h-12 opacity-50"
+                    />
+                  )}
+                  <div className="text-center text-sm">Day {day}</div>
+                </div>
+              );
+            })}
+          </div>
+
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             <div className="flex flex-col gap-4">
               <div>
@@ -113,4 +164,3 @@ const DaySixPage = () => {
 };
 
 export default DaySixPage;
-
