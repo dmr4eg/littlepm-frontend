8ff96d7326f0d076c5b6223ed6d5868f0166afe6
Damir <fifth.dmr@gmail.com>
2025-04-20 23:10:01 +0000

    instead of "project dashboard" page, make it redirect to a main page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Header = () => {
   return (
     <header className="bg-primary text-primary-foreground p-4 flex items-center justify-between">
-      <Link href="/project-dashboard">
+      <Link href="/">
         <h1 className="text-2xl font-semibold cursor-pointer">Little Project Manager</h1>
       </Link>
       <nav>
